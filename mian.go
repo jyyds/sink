28d3571f5f7f76330bda724d0b67dab7998f1a8e
jt <1293628336@qq.com>
2022-07-24 11:34:46 +0800

    添加前端代码

--- mian.go
+++ mian.go
@@ -1,22 +1,54 @@
 package main
 
 import (
+	"embed"
+	"io/fs"
+	"log"
+	"net/http"
 	"os"
+	"os/exec"
 	"os/signal"
-	"syscall"
+	"strings"
+
+	"github.com/gin-gonic/gin"
 )
 
+//go:embed frontend/dist/*
+var FS embed.FS
+
 func main() {
-	chromePath := "C:\\Users\\86158\\AppData\\Local\\Google\\Chrome\\Application"
-	// var ui lorca.UI
-	// currentDir, _ := os.Getwd()
-	// dir := filepath.Join(currentDir, ".cache")
-	// ui, _ = lorca.New("https://baidu.com", dir, 800, 600, "--disable-sync", "--disable-translate")
-	chSignal := make(chan os.Signal, 1)
-	signal.Notify(chSignal, syscall.SIGINT, syscall.SIGTERM)
-	select {
-	//case <-ui.Done():
-	case <-chSignal:
-	}
-	//ui.Close()
+	go func() {
+		gin.SetMode(gin.DebugMode)
+		router := gin.Default()
+		staticFiles, _ := fs.Sub(FS, "frontend/dist")
+		router.StaticFS("/static", http.FS(staticFiles))
+		router.NoRoute(func(c *gin.Context) {
+			path := c.Request.URL.Path
+			if strings.HasPrefix(path, "/static/") {
+				reader, err := staticFiles.Open("index.html")
+				if err != nil {
+					log.Fatal(err)
+				}
+				defer reader.Close()
+				stat, err := reader.Stat()
+				if err != nil {
+					log.Fatal(err)
+				}
+				c.DataFromReader(http.StatusOK, stat.Size(), "text/html", reader, nil)
+			} else {
+				c.Status(http.StatusNotFound)
+			}
+		})
+		router.Run(":8080")
+	}()
+
+	chromePath := "C:\\Users\\86158\\AppData\\Local\\Google\\Chrome\\Application\\chrome.exe"
+	cmd := exec.Command(chromePath, "--app=http://localhost:8080/static/")
+	cmd.Start()
+
+	chSingnal := make(chan os.Signal, 1)
+	signal.Notify(chSingnal, os.Interrupt)
+
+	<-chSingnal
+	cmd.Process.Kill()
 }
