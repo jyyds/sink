f759ed715e8fe0287787e7eb4cea3f497d4929c7
jt <1293628336@qq.com>
2022-07-24 16:29:25 +0800

    实现文件上传

--- go.mod
+++ go.mod
@@ -26,5 +26,6 @@ require (
 
 require (
 	github.com/gin-gonic/gin v1.8.1
+	github.com/google/uuid v1.3.0
 	golang.org/x/net v0.0.0-20210226172049-e18ecbb05110 // indirect
 )
--- mian.go
+++ mian.go
@@ -3,14 +3,18 @@ package main
 import (
 	"embed"
 	"io/fs"
+	"io/ioutil"
 	"log"
 	"net/http"
 	"os"
 	"os/exec"
 	"os/signal"
+	"path"
+	"path/filepath"
 	"strings"
 
 	"github.com/gin-gonic/gin"
+	"github.com/google/uuid"
 )
 
 //go:embed frontend/dist/*
@@ -21,6 +25,7 @@ func main() {
 		gin.SetMode(gin.DebugMode)
 		router := gin.Default()
 		staticFiles, _ := fs.Sub(FS, "frontend/dist")
+		router.POST("/api/v1/texts", TextsController)
 		router.StaticFS("/static", http.FS(staticFiles))
 		router.NoRoute(func(c *gin.Context) {
 			path := c.Request.URL.Path
@@ -51,4 +56,35 @@ func main() {
 
 	<-chSingnal
 	cmd.Process.Kill()
+}
+
+func TextsController(c *gin.Context) {
+	var json struct {
+		Raw string `json:"raw"`
+	}
+	if err := c.ShouldBindJSON(&json); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+	} else {
+		exe, err := os.Executable()
+		if err != nil {
+			log.Fatal(err)
+		}
+		dir := filepath.Dir(exe)
+		if err != nil {
+			log.Fatal(err)
+		}
+		filename := uuid.New().String()
+		uploads := filepath.Join(dir, "uploads")
+		err = os.MkdirAll(uploads, os.ModePerm)
+		if err != nil {
+			log.Fatal(err)
+		}
+		fullpath := path.Join("uploads", filename+".txt")
+		err = ioutil.WriteFile(filepath.Join(dir, fullpath), []byte(json.Raw), 0644)
+		if err != nil {
+			log.Fatal(err)
+		}
+		c.JSON(http.StatusOK, gin.H{"url": "/" + fullpath})
+	}
+
 }
