95a79dd1d68a7352ad187366affcc9f47278f6b4
jt <1293628336@qq.com>
2022-07-26 14:21:43 +0800

    重构main代码

--- main.go
+++ main.go
@@ -9,20 +9,29 @@ import (
 )
 
 func main() {
-	go func() {
-		server.Run()
-	}()
+	go server.Run()
+
+	cmd := startBrowser()
+
+	// 等待中断信号
+	chSingnal := listenToInterrupt()
+
+	<-chSingnal
+	cmd.Process.Kill()
+}
+
+func startBrowser() *exec.Cmd {
 
 	// 启动Chrome
 	chromePath := "C:\\Users\\86158\\AppData\\Local\\Google\\Chrome\\Application\\chrome.exe"
 	cmd := exec.Command(chromePath, "--app=http://localhost:27149/static/")
 	cmd.Start()
+	return cmd
+}
 
+func listenToInterrupt() chan os.Signal {
 	// 监听中断信号
 	chSingnal := make(chan os.Signal, 1)
 	signal.Notify(chSingnal, os.Interrupt)
-
-	// 等待中断信号
-	<-chSingnal
-	cmd.Process.Kill()
+	return chSingnal
 }
